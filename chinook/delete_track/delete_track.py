d0bd090a0f4592b9c849f6700149803e83677144
Dave Enrico <denrico@northbergen.k12.nj.us>
2020-05-14 09:14:04 -0400

    Add solution branch

--- chinook/delete_track/delete_track.py
+++ chinook/delete_track/delete_track.py
@@ -12,4 +12,9 @@ def delete_track(title):
     Deletes songs named <title> from the track table
     of the connected database. Should return None if successful
     """
-    pass
+    query = "DELETE FROM track WHERE name=?"
+    c.execute(query, (title,))
+
+delete_track("Blame It on the Rain")
+conn.commit()
+conn.close()
